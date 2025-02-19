b5d29a45b44a237d9640d41c184cf8a525058165
Milad Heidari <milad.h9999@gmail.com>
2025-02-18 23:52:42 -0800

    Fix: memory

--- controller/memory.py
+++ controller/memory.py
@@ -1,45 +1,112 @@
 import torch
+import numpy as np
 from torch.utils.data import Dataset
 
 
-class StaticDataset(Dataset):
+class ReplayBuffer:
     """
-        static dataset to hold triplets
+        Replay buffer holds sample trajectories
     """
     def __init__(
         self,
         capacity: int,
         observation_dim: int,
-        action_dim: int
+        action_dim: int,
     ):
         self.capacity = capacity
-        
-        self.observations = torch.empty((capacity, observation_dim), dtype=torch.float32)
-        self.actions = torch.empty((capacity, action_dim), dtype=torch.float32)
-        self.next_observation = torch.empty((capacity, observation_dim), dtype=torch.float32)
+
+        self.observations = np.zeros((capacity, observation_dim), dtype=np.float32)
+        self.actions = np.zeros((capacity, action_dim), dtype=np.float32)
+        self.rewards = np.zeros((capacity, 1), dtype=np.float32)
+        self.done = np.zeros((capacity, 1), dtype=bool)
 
         self.index = 0
         self.is_filled = False
 
     def __len__(self):
         return self.capacity if self.is_filled else self.index
-    
-    def __getitem__(self, index):
-        observations = self.observations[index]
-        actions = self.actions[index]
-        next_observations = self.next_observation[index]
 
-        return observations, actions, next_observations
-    
     def push(
         self,
         observation,
         action,
-        next_observation,
+        reward,
+        done,
     ):
-        self.observations[self.index] = torch.as_tensor(observation)
-        self.actions[self.index] = torch.as_tensor(action)
-        self.next_observation[self.index] = torch.as_tensor(next_observation)
+        """
+            Add experience (single step) to the replay buffer
+        """
+        self.observations[self.index] = observation
+        self.actions[self.index] = action
+        self.rewards[self.index] = reward
+        self.done[self.index] = done
 
         self.index = (self.index + 1) % self.capacity
         self.is_filled = self.is_filled or self.index == 0
+
+    def sample(
+        self,
+        batch_size: int,
+        chunk_length: int
+    ):
+        done = self.done.copy()
+        done[-1] = 1
+        episode_ends = np.where(done)[0]
+
+        all_indexes = np.arange(len(self))
+        distances = episode_ends[np.searchsorted(episode_ends, all_indexes)] - all_indexes + 1
+        valid_indexes = all_indexes[distances >= chunk_length]
+
+        sampled_indexes = np.random.choice(valid_indexes, size=batch_size)
+        sampled_ranges = np.vstack([
+            np.arange(start, start + chunk_length) for start in sampled_indexes
+        ])
+
+        sampled_observations = self.observations[sampled_ranges].reshape(
+            batch_size, chunk_length, self.observations.shape[1]
+        )
+        sampled_actions = self.actions[sampled_ranges].reshape(
+            batch_size, chunk_length, self.actions.shape[1]
+        )
+        sampled_rewards = self.rewards[sampled_ranges].reshape(
+            batch_size, chunk_length, 1
+        )
+        sampled_done = self.done[sampled_ranges].reshape(
+            batch_size, chunk_length, 1
+        )
+
+        return sampled_observations, sampled_actions, sampled_rewards, sampled_done
+    
+
+class StaticDataset(Dataset):
+
+    def __init__(
+        self,
+        replay_buffer: ReplayBuffer,
+        chunk_length: int,
+    ):
+        self.observations = torch.as_tensor(replay_buffer.observations)
+        self.actions = torch.as_tensor(replay_buffer.actions)
+        self.rewards = torch.as_tensor(replay_buffer.rewards)
+        self.done = torch.as_tensor(replay_buffer.done).clone()
+        self.chunk_length = chunk_length
+
+        # compute valid indexes
+        self.done[-1] = 1
+        episode_ends = torch.where(self.done)[0]
+
+        all_indexes = torch.arange(self.observations.shape[0])
+        distances = episode_ends[torch.searchsorted(episode_ends, all_indexes)] - all_indexes + 1
+        self.valid_indexes = all_indexes[distances >= self.chunk_length]
+
+    def __len__(self):
+        return len(self.valid_indexes)
+    
+    def __getitem__(self, index):
+        valid_index = self.valid_indexes[index]
+
+        observations = self.observations[valid_index: valid_index + self.chunk_length]
+        actions = self.actions[valid_index: valid_index + self.chunk_length]
+        rewards = self.rewards[valid_index: valid_index + self.chunk_length]
+        
+        return observations, actions, rewards
---
+++ controller/models.py
@@ -0,0 +1,161 @@
+import torch
+import torch.nn as nn
+from typing import Optional
+from torch.distributions import Normal
+
+
+class ObservationModel(nn.Module):
+    """
+        p(o_t | s_t)
+        constant variance
+    """
+    def __init__(
+        self,
+        state_dim: int,
+        observation_dim: int,
+        hidden_dim: Optional[int]=None,
+    ):
+        super().__init__()
+
+        hidden_dim = hidden_dim if hidden_dim is not None else state_dim * 2
+        
+        self.mlp_layers = nn.Sequential(
+            nn.Linear(state_dim, hidden_dim),
+            nn.ReLU(),
+            nn.Linear(hidden_dim, hidden_dim),
+            nn.ReLU(),
+            nn.Linear(hidden_dim, observation_dim),
+        )
+
+    def forward(self, state):
+        return self.mlp_layers(state)
+    
+
+class RewardModel(nn.Module):
+    """
+        p(r_t | s_t, o*)
+        constant variance
+        Note: conditioning on the goal
+    """
+    def __init__(
+        self,
+        state_dim: int,
+        observation_dim: int,
+        hidden_dim: Optional[int]=None,
+    ):
+        super().__init__()
+
+        hidden_dim = hidden_dim if hidden_dim is not None else 2*(state_dim + observation_dim)
+
+        self.mlp_layers = nn.Sequential(
+            nn.Linear(state_dim + observation_dim, hidden_dim),
+            nn.ReLU(),
+            nn.Linear(hidden_dim, hidden_dim),
+            nn.ReLU(),
+            nn.Linear(hidden_dim, 1),
+        )
+
+    def forward(self, state, goal):
+        return self.mlp_layers(
+            torch.cat([state, goal], dim=1)
+        )
+    
+
+class TransitionModel(nn.Module):
+    """
+        p(s_t | s_{t-1}, a_{t-1})
+    """
+    def __init__(
+        self,
+        state_dim: int,
+        action_dim: int,
+        hidden_dim: Optional[int]=None,
+        min_std: float=1e-2,
+    ):
+        super().__init__()
+
+        hidden_dim = (
+            hidden_dim if hidden_dim is not None else 2*(state_dim + action_dim)
+        )
+
+        self.mlp_layers = nn.Sequential(
+            nn.Linear(state_dim + action_dim, hidden_dim),
+            nn.ReLU(),
+            nn.Linear(hidden_dim, hidden_dim),
+            nn.ReLU(),
+        )
+
+        self.mean_head = nn.Linear(hidden_dim, state_dim)
+        self.std_head = nn.Sequential(
+            nn.Linear(hidden_dim, state_dim),
+            nn.Softplus(),
+        )
+
+        self._min_std = min_std
+
+    def forward(self, prev_state, prev_action):
+        hidden = self.mlp_layers(
+            torch.cat([prev_state, prev_action], dim=1)
+        )
+        mean = self.mean_head(hidden)
+        std = self.std_head(hidden) + self._min_std
+        prior_dist = Normal(mean, std)
+        return prior_dist
+    
+
+class PosteriorModel(nn.Module):
+    """
+        q(s_t | o1:t, a1:t-1)
+    """
+
+    def __init__(
+        self,
+        observation_dim: int,
+        action_dim: int,
+        state_dim: int,
+        rnn_hidden_dim: int,
+        rnn_input_dim: int,
+        min_std: float=1e-2,
+    ):
+        super().__init__()
+
+        # RNN hidden at time t summarizes o1:t-1, a1:t-2
+        self.rnn = nn.GRUCell(
+            input_size=rnn_input_dim,
+            hidden_size=rnn_hidden_dim,
+        )
+
+        self.fc_obs_action = nn.Sequential(
+            nn.Linear(observation_dim + action_dim, rnn_input_dim),
+            nn.ReLU(),
+        )
+
+        self.posterior_mean_head = nn.Linear(rnn_hidden_dim, state_dim)
+        self.posterior_std_head = nn.Sequential(
+            nn.Linear(rnn_hidden_dim, state_dim),
+            nn.Softplus(),
+        )
+
+        self.rnn_hidden_dim = rnn_hidden_dim
+        self.state_dim = state_dim
+        self.action_dim = action_dim
+        self.rnn_input_dim = rnn_input_dim
+        self._min_std = min_std
+
+    def forward(
+        self,
+        prev_rnn_hidden,
+        prev_action,
+        observation,
+    ):
+        rnn_input = self.fc_obs_action(
+            torch.cat([observation, prev_action], dim=1)
+        )
+        rnn_hidden = self.rnn(rnn_input, prev_rnn_hidden)
+
+        posterior_mean = self.posterior_mean_head(rnn_hidden)
+        posterior_std = self.posterior_std_head(rnn_hidden) + self._min_std
+
+        posterior_dist = Normal(posterior_mean, posterior_std)
+
+        return rnn_hidden, posterior_dist
